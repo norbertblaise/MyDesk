1836803fefb8e11f9fdc2b84026a73dbf2f70c13
norbertblaise <lukwayi@gmail.com>
2022-06-11 23:06:42 +0300

    firebase auth added

--- app/src/main/java/com/my/desk/MainActivity.kt
+++ app/src/main/java/com/my/desk/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
